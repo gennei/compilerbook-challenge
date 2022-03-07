7877f811809433ed589118a961e99cc9a5fd716e
gennei <sai.gennei@gmail.com>
2022-03-07 16:38:39 +0900

    code format by c/c++ ext pack

--- 9cc.c
+++ 9cc.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
