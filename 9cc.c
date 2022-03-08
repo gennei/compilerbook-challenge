7cdf41c588463f176465303fadf297c7067bb3ec
gennei <sai.gennei@gmail.com>
2022-03-08 15:42:56 +0900

    user input のエラー詳細を表示する関数を実装

--- 9cc.c
+++ 9cc.c
@@ -27,12 +27,29 @@ struct Token
 // 現在着目しているトークン
 Token *token;
 
+// 入力プログラム
+char *user_input;
+
 // エラーを報告するための関数
 // printfと同じ引数を取る
 void error(char *fmt, ...)
 {
   va_list ap;
   va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -53,7 +70,7 @@ bool consume(char op)
 void expect(char op)
 {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
 
@@ -62,7 +79,7 @@ void expect(char op)
 int expect_number()
 {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -126,6 +143,7 @@ int main(int argc, char **argv)
     error("引数の個数が正しくありません");
     return 1;
   }
+  user_input = argv[1];
 
   // トークナイズする
   token = tokenize(argv[1]);
